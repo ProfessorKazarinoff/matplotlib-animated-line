76769723eea1c4c4ec62c0e0e508c6a31f0672be
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-02-26 17:34:47 -0800

    added Corey Schafer code from his github repo

---
+++ generate_data.py
@@ -0,0 +1,42 @@
+# generate_data.py
+
+"""
+from Corey Schafer
+https://www.youtube.com/watch?v=Ercd-Ip5PfQ
+https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Matplotlib/09-LiveData/data_gen.py
+"""
+
+import csv
+import random
+import time
+
+x_value = 0
+total_1 = 1000
+total_2 = 1000
+
+fieldnames = ["x_value", "total_1", "total_2"]
+
+
+with open('data.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+while True:
+
+    with open('data.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        info = {
+            "x_value": x_value,
+            "total_1": total_1,
+            "total_2": total_2
+        }
+
+        csv_writer.writerow(info)
+        print(x_value, total_1, total_2)
+
+        x_value += 1
+        total_1 = total_1 + random.randint(-6, 8)
+        total_2 = total_2 + random.randint(-5, 6)
+
+    time.sleep(1)
